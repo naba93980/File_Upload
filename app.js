7a1cde6505e9969147e19eb443e4142fa5aae37b
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-11-01 01:11:16 +0530

    added middlewares and routes

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 require('dotenv').config();
 require('express-async-errors');
 
+const fileUpload = require('express-fileupload');
 const express = require('express');
 const app = express();
 
 // database
 const connectDB = require('./db/connect');
 
+// product router
+const productRouter = require('./routes/productRoutes');
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.get('/', (req, res) => {
-  res.send('<h1>File Upload Starter</h1>');
-});
 
-// middleware
+app.use(express.static('./public'))
+app.use(express.json());
+app.use(fileUpload());
+app.use('/products', productRouter);
+
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
